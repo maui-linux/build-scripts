834ede54fe946808300d8dd0e9dc2d7e6bbed5ad
Leszek Lesner <leszek.lsner@web.de>
2015-04-24 11:35:03 +0200

    Don't forget installing the keyring file

--- remaster-kit-build/addsourceslist.sh
+++ remaster-kit-build/addsourceslist.sh
@@ -6,6 +6,8 @@ if [ -e edit ] && [ -e $BUILD_DIR/sourceslist.deb ]; then
 	cp $BUILD_DIR/sourceslist.deb edit/tmp/sourceslist.deb
 	exec_in_chroot "dpkg -i /tmp/sourceslist.deb"
 	exec_in_chroot "rm /etc/apt/sources.list" # Make sure to remove that so we don't have duplicates
+	cp $BUILD_DIR/keyring.deb edit/tmp/keyring.deb
+	exec_in_chroot "dpkg -i /tmp/keyring.deb"
 	exec_in_chroot "apt-get update"
 	rm edit/tmp/sourceslist.deb
 fi
