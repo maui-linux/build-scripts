b1286967b37fceed83b8f1bfeaf68d2cd3136a6c
Leszek Lesner <leszek.lsner@web.de>
2015-04-24 10:47:42 +0200

    Install correct base-files version for Netrunner 16 Remove sources.list after sourceslist.deb is installed to avoid duplicates Fixed typo

--- remaster-kit-build/addsourceslist.sh
+++ remaster-kit-build/addsourceslist.sh
@@ -5,6 +5,7 @@ source /usr/share/remaster-kit/functions
 if [ -e edit ] && [ -e $BUILD_DIR/sourceslist.deb ]; then
 	cp $BUILD_DIR/sourceslist.deb edit/tmp/sourceslist.deb
 	exec_in_chroot "dpkg -i /tmp/sourceslist.deb"
+	rm edit/etc/apt/sources.list # Make sure to remove that so we don't have duplicates
 	exec_in_chroot "apt-get update"
 	rm edit/tmp/sourceslist.deb
 fi
