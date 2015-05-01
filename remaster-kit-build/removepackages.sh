f136f8a76c33c6f85292817d15eececd2fa9fbbb
Leszek Lesner <leszek.lsner@web.de>
2015-05-01 10:14:31 +0200

    Fix whitespace handling in packages add remove lists

--- remaster-kit-build/removepackages.sh
+++ remaster-kit-build/removepackages.sh
@@ -3,7 +3,7 @@
 source /usr/share/remaster-kit/functions
 
 if [ -e $BUILD_DIR/packages-remove.txt ]; then
-	packages_to_add=`cat $BUILD_DIR/packages-remove.txt | tr -d '\n'`
+	packages_to_add=`cat $BUILD_DIR/packages-remove.txt | tr -t '\n' ' '`
 	exec_in_chroot "apt-get purge $packages_to_add"
         exec_in_chroot "apt-get autoremove"
 fi
