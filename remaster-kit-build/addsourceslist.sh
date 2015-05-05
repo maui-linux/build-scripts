60dcdd4e9f44b8de068d07838263ad1f6e4be55a
Leszek Lesner <leszek.lsner@web.de>
2015-05-05 10:20:59 +0200

    Use /dev/null to clear sources.list

--- remaster-kit-build/addsourceslist.sh
+++ remaster-kit-build/addsourceslist.sh
@@ -5,7 +5,7 @@ source /usr/share/remaster-kit/functions
 if [ -e edit ] && [ -e $BUILD_DIR/sourceslist.deb ]; then
 	cp $BUILD_DIR/sourceslist.deb edit/tmp/sourceslist.deb
 	exec_in_chroot "dpkg -i /tmp/sourceslist.deb"
-	exec_in_chroot "echo > /etc/apt/sources.list" # Make sure to remove that so we don't have duplicates
+	exec_in_chroot "cat /dev/null > /etc/apt/sources.list" # Make sure to clear that so we don't have duplicates
 	cp $BUILD_DIR/keyring.deb edit/tmp/keyring.deb
 	exec_in_chroot "dpkg -i /tmp/keyring.deb"
 	exec_in_chroot "apt-get update"
